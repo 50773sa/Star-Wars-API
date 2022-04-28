ba973e4d1a3a144a05d92a840200503df0121ed8
50773sa <93046590+50773sa@users.noreply.github.com>
2022-04-28 19:42:18 +0200

    Added {Link} to films

--- src/components/SinglePerson.jsx
+++ src/components/SinglePerson.jsx
@@ -2,15 +2,17 @@ import React, { useEffect } from 'react'
 import { Container } from "react-bootstrap"
 import { ListGroup, Card, Button } from "react-bootstrap"
 import { getIdFromUrl} from "../helpers"
-import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useParams, useNavigate } from 'react-router-dom'
 
 // styles
 import '../App.css'
 
-function SinglePerson({ person, id }) {
+function SinglePerson({ person, film }) {
     const navigate = useNavigate()
-  
+    const { id } = useParams()
 
+  
+console.log('FILM:', film)
     return (
         <>
             <Container>
@@ -27,19 +29,17 @@ function SinglePerson({ person, id }) {
                         <ListGroup.Item><strong>Skin Color </strong>{person.skin_color}</ListGroup.Item>
                         <ListGroup.Item><strong>Eye Color </strong>{person.eye_color}</ListGroup.Item>     
                                        
-                        <ListGroup.Item>Links</ListGroup.Item>
-                        <ListGroup.Item><strong>Films </strong>{person.films}</ListGroup.Item>
-                    </ListGroup>
+                        <ListGroup.Item><strong>Films </strong></ListGroup.Item>
 
-                          {/* Links to films */}
-                          {person.films && 
-                            person.films.map((person, id) => {
-                                return (
-                                    <a href='{person}' key={id} value={person} to={`/people/${person.id}`} >
-                                        <ListGroup.Item>Films {`${getIdFromUrl(person)}`} </ListGroup.Item>    
-                                    </a> 
-                            )
-                        })} 
+                            {/* Links to films */}
+                            {person.films && 
+                                person.films.map((person, id) => {
+                                    return (
+                                            <ListGroup.Item key={id} value={person} as={Link} to={`/films/${id}`}>Films {`${getIdFromUrl(person)}`} ≫</ListGroup.Item>    
+                                )
+                            })} 
+                        </ListGroup>
+
 
                     <div className="m-3">
                         <Button variant="warning" size="sm" onClick={() => navigate(-1)}>≪ Back</Button>   
