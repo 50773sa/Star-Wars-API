d301900983c8a631fc1cf850f0522851903e7384
50773sa <93046590+50773sa@users.noreply.github.com>
2022-04-29 16:57:09 +0200

    Two 'link buttons' -  /film and /people

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom"
 
+const HomePage = ({ films }) => {
 
-const HomePage = () => {
+	console.log(Link)
 	return (
 		<div className='homepageBtnsWrapper'>
-			<div className="homepageBtns homepageBtnsOne"></div>
-			<div className="homepageBtns homepageBtnsTwo"></div>
+				<div className="homepageBtns homepageBtnsOne">
+					<Link to={`/films`}>
+						<h2>Films</h2>
+					</Link>
+				</div>
+			
+				<div className="homepageBtns homepageBtnsTwo">
+					<Link to={`/people`}>
+						<h2>People</h2>
+					</Link>
+				</div>
 		</div>
 		
 	)
