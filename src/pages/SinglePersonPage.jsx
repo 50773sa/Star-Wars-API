829a7267c5b3f2ef02affb41b933338c7bd74403
50773sa <93046590+50773sa@users.noreply.github.com>
2022-04-29 12:49:28 +0200

    Added Loading... & try/catch

--- src/pages/SinglePersonPage.jsx
+++ src/pages/SinglePersonPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import StarWarsAPI from "../services/StarWarsAPI"
@@ -7,24 +6,48 @@ import SinglePerson from '../components/SinglePerson'
 function SinglePersonPage() {
     const [person, setPerson] = useState([])
     const { id } = useParams()
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [isError, setIsError] = useState(false)
 
     const getPerson = async (id) => {
-		// get person from StarWarsAPI
-		const data = await StarWarsAPI.getSinglePerson(id)
-		// update person state
-        console.log(data.id)
-		setPerson(data)
+        setIsLoading(true)
+
+        try {
+            // get person from StarWarsAPI
+            const data = await StarWarsAPI.getSinglePerson(id)
+
+            // fake slow api
+            await new Promise(r => setTimeout(r, 1500))
+
+            // update person state
+            setPerson(data)
+
+            // reset any previos error
+            setIsError(false)
+            setError(null)
+        } catch (err) {
+            setIsError(false)
+            setError(err)
+        } finally {
+            setIsLoading(false)
+        }
+
+		
  	}
      console.log('PERSON', person)
 
     // get person from api when component i mounted 
     useEffect(() => {
         getPerson(id)
-    }, [id])  
-
+    }, [id])
+    
     return (
-        <div className='singlePerson'>
-            {person && (
+        <div>
+            {isLoading && (<p>Loading...</p>)}
+			{isError && (<p><strong>ERROR!</strong> Sorry, an error has occured: {error.message}</p>)}
+
+            {person && !isLoading && (
                 <SinglePerson person={person}/>
             )}
         </div>
