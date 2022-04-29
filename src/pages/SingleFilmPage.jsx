07f0b5c79522cce4fe1690bcd4d9de0bd7597267
50773sa <93046590+50773sa@users.noreply.github.com>
2022-04-29 12:55:37 +0200

    Added try/catch & Loading...

--- src/pages/SingleFilmPage.jsx
+++ src/pages/SingleFilmPage.jsx
@@ -1,35 +1,59 @@
-import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import StarWarsAPI from "../services/StarWarsAPI"
 import SingleFilm from '../components/SingleFilm'
+import StarWarsAPI from "../services/StarWarsAPI"
 
 
 function SingleFilmPage() {
     const [film, setFilm] = useState([])
     const { id } = useParams()
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [isError, setIsError] = useState(false)
 
     const getFilm = async (id) => {
-		// get film from StarWarsAPI
-		const data = await StarWarsAPI.getSingleFilm(id)
-		// update film state
-		setFilm(data)
- 	}
-    // console.log('FILM', film)
+        setIsLoading(true)
 
-    // get film from api when component i mounted 
+        try {
+            // get film from StarWarsAPI
+            const data = await StarWarsAPI.getSingleFilm(id)
+
+            // fake slow api
+            await new Promise(r => setTimeout(r, 1500))
+
+            // update film state
+            setFilm(data)
+
+            // reset any previos error
+            setIsError(false)
+            setError(null)
+
+        } catch (err) {
+            setIsError(false)
+            setError(err)
+
+        } finally {
+            setIsLoading(false)
+        } 
+ 	}
+
+    // get film from api when component is mounted 
     useEffect(() => {
         getFilm(id)
     }, [id])  
 
   
-  return (
-    <div className='singleFilm'>
-        {film && (
-              <SingleFilm film={film} />
-        )}
-    </div>
-  )
+    return (
+        <div className='singleFilm'>
+
+            {isLoading && (<p>Loading...</p>)}
+			{isError && (<p><strong>ERROR!</strong> Sorry, an error has occured: {error.message}</p>)}
+
+            {film && !isLoading && (
+                <SingleFilm film={film} />
+            )}
+        </div>
+    )
 }
 
 export default SingleFilmPage
