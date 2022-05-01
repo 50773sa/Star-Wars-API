207f1a8b835b605c9cb323dfae797fe2c9860771
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-01 10:22:19 +0200

    New pagination

--- src/pages/AllPeoplePage.jsx
+++ src/pages/AllPeoplePage.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react'
 import AllPeopleList from '../components/AllPeopleList'
 import LoadingSpinner from '../components/LoadingSpinner'
-import Pagination from '../components/Pagination'
 import StarWarsAPI from "../services/StarWarsAPI"
+import { Button, Container } from 'react-bootstrap'
 
-import '../App.css'
+
 
 function AllPeoplePage() {
     const [people, setPeople] = useState([])
-    const [currentPage, setCurrentPage] = useState(1)   
-    const [peoplePerPage] = useState(9) 
+    const [page, setPage] = useState(1)   
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(null)
     const [isError, setIsError] = useState(false)
@@ -19,7 +18,7 @@ function AllPeoplePage() {
 
         try {
             // get people from StarWarsAPI
-            const data = await StarWarsAPI.getAllPeople()
+            const data = await StarWarsAPI.getAllPeople(page)
 
             // fake slow api
             await new Promise(r => setTimeout(r, 1500))
@@ -38,33 +37,44 @@ function AllPeoplePage() {
 
     // get people from api when component i mounted 
     useEffect(() => {
-        getPeople()
-    }, []) 
+        getPeople(page)
+    }, [page]) 
 
-     // number of cards per page
-     const indexOfLastPerson = currentPage * peoplePerPage
-     const indexOfFirstPerson = indexOfLastPerson - peoplePerPage
-     const peopleOnDisplay = people.slice(indexOfFirstPerson, indexOfLastPerson)
-
-     const paginate = pageNumber => setCurrentPage(pageNumber)
  
-    console.log('PEOPLE', peopleOnDisplay)
-    console.log(currentPage)
-	 
     return (
         
         <div className='allPeople'>
             {isLoading && (<LoadingSpinner />)}
 			{isError && (<p><strong>ERROR!</strong> Sorry, an error has occured: {error.message}</p>)}
 
-            
-            {people && !isLoading && (
-                <div>
-                    <AllPeopleList people={peopleOnDisplay}/>
-                    <Pagination peoplePerPage={peoplePerPage} people={people.length} paginate={paginate} />
-                </div>
+            {people && people.results &&!isLoading && (
+                <AllPeopleList people={people}/>
             )}  
-        </div>
+
+            {page && (
+                <Container className='pageBtns'>
+                    <div className="previouBtn">
+                        <Button
+                            variant="dark"
+                            onClick={() => setPage(value => value -1)}
+                            disabled={!people.previous}
+                        >
+                            Previous
+                        </Button>
+                    </div>
+       
+                    <div className="nextBtn">
+                        <Button
+                            variant="dark"
+                            onClick={() => setPage(value => value +1)}
+                            disabled={!people.next}
+                        >
+                            Next
+                        </Button>
+                    </div>
+                </Container>
+            )}
+        </div>  
     )
 }
 
