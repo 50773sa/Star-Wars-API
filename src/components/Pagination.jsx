67f23d3d2f9e50860b64cd05c1c0e67b77e454cf
50773sa <93046590+50773sa@users.noreply.github.com>
2022-05-01 13:49:09 +0200

    Deleted pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ListGroupItem } from 'react-bootstrap'
+import { Button, Container, ListGroup } from 'react-bootstrap'
+import ReactPaginate from 'react-paginate'
+import { useEffect, useState } from 'react'
+
+
+import '../App.css'
+
+
+function Pagination({ people }) {
+
+
+  
+  return (
+      <div>
+            
+      
+      </div>   
+  )
+}
+
+export default Pagination
